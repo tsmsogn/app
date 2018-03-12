c1f95bf388ac84a11b0eb6933a91be9e3c39bcd8
tsmsogn <tsmsogn@gmail.com>
2018-03-12 12:06:10 +0900

    Update travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,17 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+use Cake\Datasource\ConnectionManager;
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
 $_SERVER['PHP_SELF'] = '/';
+
+// Ensure default test connection is defined
+if (!getenv('db_dsn')) {
+    putenv('db_dsn=sqlite:///:memory:');
+}
+
+ConnectionManager::setConfig('test', ['url' => getenv('db_dsn')]);
